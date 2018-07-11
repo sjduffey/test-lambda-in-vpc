2e1a5bbdcd295a390ffd484e56f1302b061fb4f0
Stewart Duffey <stewart.duffey@dvsa.gov.uk>
2018-07-11 16:02:39 +0100

    Select stuff from a db

--- handler.ts
+++ handler.ts
@@ -1,13 +1,26 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import { createConnection, QueryError, RowDataPacket } from 'mysql';
 
 export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
 
-  cb(null, response);
-}
+    const db = {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    };
+
+    const connection = createConnection(db);
+
+    connection.query('SELECT * FROM random_names', (err: QueryError, rows: RowDataPacket[]) => {
+
+        const names = [ rows[0]['name'], rows[1]['name'], rows[2]['name'] ];
+
+        // const response = {
+        //     statusCode: 200,
+        //     body: JSON.stringify(names),
+        // };
+
+        cb(null, JSON.stringify(names));
+    });
+};
